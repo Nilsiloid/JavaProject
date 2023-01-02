403cccefcca81e07e1a70f0b1002e9de1f8d22c6
Shlok Agrawal <46015054+agrawalshlok@users.noreply.github.com>
2023-01-02 20:52:37 +0530

    Add files via upload

---
+++ PortalMain.cpp
@@ -0,0 +1,28 @@
+#include "demo/DemoPortal.h"
+#include <iostream>
+#include <fstream>
+using namespace std;
+class PortalMain {
+    public:
+    static void Main() {
+        DemoPortal demoportal;
+        while(1) {
+            string data;
+            getline(std::cin, data);
+            if(data == "end") {
+                break;
+            } else {
+                demoportal.processUserCommand(data);
+            }
+        }
+    }
+};
+int main() {
+    //PortalMain::Main();
+    DemoPortal dportal;
+    dportal.processUserCommand("Buy s 1");
+    dportal.processUserCommand("Buy s 2");
+    dportal.processUserCommand("List Book Price");
+    dportal.processUserCommand("List Book Name");
+    dportal.checkResponse();
+}
