82605e5b6a6255e59a164e7a8e9fed22f5bfcc51
Nilay <92648000+Nilsiloid@users.noreply.github.com>
2023-01-02 23:06:12 +0530

    Added PlatformMain file.

---
+++ PlatformMain.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+import demo.*;
+import ecomm.*;
+
+public class PlatformMain {
+
+	public static void main(String[] args) {
+
+		Platform pf = new DemoPlatform(); // replace with appropriate derived class
+
+		// create a number of sellers (of different sub-types of Seller)
+		// Assign a name (sellerID) to each of them.
+
+		// replace each of the XYZ below with the derived class name of one of the
+		// team members.
+
+		Scanner sc = new Scanner(System.in);
+
+		Seller s1 = new Nilay("Nilay");
+		s1.addPlatform(pf);
+
+		Seller s2 = new Madhav("Madhav");
+		s2.addPlatform(pf);
+
+		Seller s3 = new Shlok("Shlok");
+		s3.addPlatform(pf);
+
+		/*
+		 * // keep reading from System.in
+		 * // If "Check" typed in, invoke
+		 * pf.processRequests();
+		 */
+
+		while (true) {
+			if (sc.next().equals("Check")) {
+				pf.processRequests();
+			}
+		}
+	}
+
+}
