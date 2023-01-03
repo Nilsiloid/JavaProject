3f1b71b92f4bb3dd52f293eeaecd375bc9f35cbb
Madhav Sood <madhav.sood0407@gmail.com>
2023-01-03 15:10:39 +0530

    Fixed

--- demo/DemoPlatform.java
+++ demo/DemoPlatform.java
@@ -20,57 +20,56 @@ public class DemoPlatform extends Platform {
 	
 	@Override
 	public void processRequests() {
-		// File tempFile = new File("PortalToPlatform.txt");
-		// if(tempFile.exists() == false) {
-		// 	return;
-		// }
-		try {
-			BufferedReader br = new BufferedReader(new FileReader("PortalToPlatform.txt"));
-			BufferedWriter bw = new BufferedWriter(new FileWriter("PlatformToPortal.txt", true));
-			String line;
-			Globals g = new Globals();
-			while ((line = br.readLine()) != null) {
-				String[] order = line.split(" ");
+		File tempFile = new File("PortalToPlatform.txt");
+		if(tempFile.exists()){
+			try {
+				BufferedReader br = new BufferedReader(new FileReader("PortalToPlatform.txt"));
+				BufferedWriter bw = new BufferedWriter(new FileWriter("PlatformToPortal.txt", true));
+				String line;
+				Globals g = new Globals();
+				while ((line = br.readLine()) != null) {
+					String[] order = line.split(" ");
 
-				String output = order[0] + " " + order[1] + " "; // Portal ID + Request ID
+					String output = order[0] + " " + order[1] + " "; // Portal ID + Request ID
 
-				if (order[2].equals("Start")) {
-					BufferedWriter bwTemp = new BufferedWriter(new FileWriter("PlatformToPortal.txt"));
-					bwTemp.write("");
-					output += g.getCategoryName(Category.Mobile) + " " + g.getCategoryName(Category.Book);
-					bw.write(output + "\n");
-				} else if (order[2].equals("List")) {
-					for (int i = 0; i < sellers.size(); i++) {
-						ArrayList<Product> temp = new ArrayList<Product>();
-						temp = sellers.get(i).findProducts(order[3].equals("Book") ? Category.Book : Category.Mobile);
+					if (order[2].equals("Start")) {
+						BufferedWriter bwTemp = new BufferedWriter(new FileWriter("PlatformToPortal.txt"));
+						bwTemp.write("");
+						output += g.getCategoryName(Category.Mobile) + " " + g.getCategoryName(Category.Book);
+						bw.write(output + "\n");
+					} else if (order[2].equals("List")) {
+						for (int i = 0; i < sellers.size(); i++) {
+							ArrayList<Product> temp = new ArrayList<Product>();
+							temp = sellers.get(i).findProducts(order[3].equals("Book") ? Category.Book : Category.Mobile);
 
-						for (int j = 0; j < temp.size(); j++) {
-							String product = output + temp.get(j).getName() + " " + temp.get(j).getProductID() + " "
-									+ temp.get(j).getPrice() + " " + temp.get(j).getQuantity();
-							bw.write(product + "\n");
-						}
-					}
-				} else if (order[2].equals("Buy")) {
-					for (int i = 0; i < sellers.size(); i++) {
-						if (order[3].contains(sellers.get(i).getID())) {
-							if (sellers.get(i).buyProduct(order[3], Integer.valueOf(order[4])) == true) {
-								output += "Success";
-							} else {
-								output += "Failure";
+							for (int j = 0; j < temp.size(); j++) {
+								String product = output + temp.get(j).getName() + " " + temp.get(j).getProductID() + " "
+										+ temp.get(j).getPrice() + " " + temp.get(j).getQuantity();
+								bw.write(product + "\n");
 							}
 						}
+					} else if (order[2].equals("Buy")) {
+						for (int i = 0; i < sellers.size(); i++) {
+							if (order[3].contains(sellers.get(i).getID())) {
+								if (sellers.get(i).buyProduct(order[3], Integer.valueOf(order[4])) == true) {
+									output += "Success";
+								} else {
+									output += "Failure";
+								}
+							}
+						}
+						bw.write(output + "\n");
 					}
-					bw.write(output + "\n");
 				}
-			}
-			br.close();
-			bw.close();
-			BufferedWriter bw1 = new BufferedWriter(new FileWriter("PortalToPlatform.txt"));
-			bw1.write("");
-			bw1.close();
+				br.close();
+				bw.close();
+				BufferedWriter bw1 = new BufferedWriter(new FileWriter("PortalToPlatform.txt"));
+				bw1.write("");
+				bw1.close();
 
-		} catch (IOException e) {
-			e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
